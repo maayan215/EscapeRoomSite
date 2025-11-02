c9a3263f77b900aafa0607e86b0fff34b032c52c
maayan215 <maayan.aharonn@gmail.com>
2025-11-02 12:00:22 +0200

    publish

--- test.py
+++
@@ -1,8 +0,0 @@
-import CalendarFunctions
-date = "2025-10-16"
-time = "14:30"
-
-
-
-# holder = CalendarFunctions.get_Specific_Event(date, time)
-# CalendarFunctions.service.events().delete(calendarId=CalendarFunctions.calenderID, eventId=holder[0]['id']).execute()
