4ab081548a9f2c255aba6afc5357e4840d3c451f
maayan215 <maayan.aharonn@gmail.com>
2025-11-09 10:34:39 +0200

    main changes

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from nicegui import ui
 
 ui.label('Hello, Escape Room Site!')
 ui.button('Click me', on_click=lambda: ui.notify('Button clicked!'))
-ui.run()
+
+
+if __name__ in {"__main__", "__mp_main__"}:
+    ui.run()
